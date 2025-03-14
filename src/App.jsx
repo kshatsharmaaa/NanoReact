7ee8ac5052185f613a4c4a20c888c6d029c810ba
akshat <kshatsharmaaa@gmail.com>
2025-03-14 13:56:31 +0530

    accordian

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import Accordian from './components/accordian'
 
 function App() {
 
 
   return (
     <>
-      
-      
+    {/* <h2>Nanoreact!</h2> */}
+      <Accordian/>
     </>
   )
 }
---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,46 @@
+
+import { useState } from "react"
+import data from "./data";
+import "./style.css"
+
+// single selection
+// multiple selection
+
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+    const [multiSelection, setMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    function handleMultiSelection(getCurrentId) {
+        let copyMultiple = [...multiple];
+        const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+        if(findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId);
+        else copyMultiple.splice(findIndexOfCurrentId, 1); // rremove
+        setMultiple(copyMultiple);
+    }
+
+    return <div className="wrapper">
+        <h3>#1 Accordian</h3>
+        <button onClick={() =>  setMultiSelection(!multiSelection)}>Enable Mutli-Selection</button>
+        <div className="accordian">
+            {data && data.length > 0 ?
+                data.map(dataItem => <div className="item">
+                    <div className="title" onClick={multiSelection 
+                        ? () => handleMultiSelection(dataItem.id) 
+                        : () => handleSingleSelection(dataItem.id)}>
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                    {selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? <div className="content">{dataItem.answer}</div> : null}
+                </div>
+                )
+                : <div className="">No data found!</div> }
+        </div>
+    </div>
+} 
